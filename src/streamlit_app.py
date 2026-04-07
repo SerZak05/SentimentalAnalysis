1367d73858fed37159256c1ce10a17642cd3fc3f
SerZak05 <serzakand@gmail.com>
2026-04-08 02:49:57 +0300

    Changed entrypoint

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,40 +1,75 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import folium
+from streamlit_folium import st_folium
+from collections import defaultdict
+# from joblib import Parallel, delayed
+from post_assesment import get_sentiment, emotions
+from post_search import search_posts_parallel
 
-"""
-# Welcome to Streamlit!
+emotion_to_color = {
+    'no_emotion': "#666666",
+    'joy': "#33cc33",
+    'sadness': "#0066ff",
+    'surprise': "#ff9900",
+    'fear': "#aa2fd6",
+    'anger': "#ff0000"
+}
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+POSTS_CNT = 500
+# TOP_CITIES_CNT = 2
+NUM_OF_WORKERS = 8
+# top_cities = cities_db.nlargest(n=TOP_CITIES_CNT, columns="population")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# === Beginning of the page ===
+st.title("Sentiment analysis")
+topic = st.text_input("Enter your topic:", "котики")
+button = st.button("Start!")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+if button:
+    st.session_state["running"] = True
+    st.session_state.pop("results", None)
+    st.session_state.pop("posts", None)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if st.session_state.get("running", False):
+    st.text("Processing query...")
+    st.session_state["posts"] = search_posts_parallel(topic, POSTS_CNT)
+    # posts_per_city = Parallel(n_jobs=NUM_OF_WORKERS) \
+    # (
+    #     delayed(search_posts_by_pos)(topic, POSTS_CNT, city_row["city"], city_row["lat"], city_row["lon"]) 
+    #     for ind, city_row in top_cities.iterrows()
+    # )
+    # posts = [post for city_list in posts_per_city for post in city_list]
+    # print(*[post.owner_id for post in posts], sep='\n', flush=True)
+    # st.session_state["posts"] = posts
+    st.text("Gathered posts...")
+    st.session_state["results"] = get_sentiment(st.session_state["posts"])
+    # st.write(st.session_state["results"])
+    st.session_state["running"] = False
+    
+if "results" in st.session_state:
+    print("Got results!", flush=True)
+    posts = st.session_state["posts"]
+    results = st.session_state["results"]
+    scores = defaultdict(lambda: {e: 0.0 for e in emotions})
+    cnt = defaultdict(int)
+    names = {}
+    for i in range(len(posts)):
+        pos = posts[i].geolocation
+        names[pos] = posts[i].city_of_origin
+        cnt[pos] += 1
+        # for label, score in results[i].items():
+        #     scores[pos][label] = score
+        scores[pos][results[i]["label"]] = results[i]["score"] if results[i]["label"] != "no_emotion" else 0.001
+    colors = {pos: emotion_to_color[max(score, key=score.get)] for pos, score in scores.items()}
+    map_table = {
+        "lon": [pos[0] for pos in cnt.keys()],
+        "lat": [pos[1] for pos in cnt.keys()],
+        "color": colors,
+        "size": cnt.values()
+    }
+    m = folium.Map()
+    for pos in cnt.keys():
+        # print(pos)
+        folium.CircleMarker((float(pos[0]), float(pos[1])), radius=cnt[pos] / POSTS_CNT * 100, color=colors[pos]).add_to(m)
+    st_folium(m, width=725, returned_objects=[])
+    # st.map(map_table, latitude="lat", longitude="lon", color="color", size="size")
